bf835d52275c3993c4b1c52e004cd87f932be384
TamirBenSimon <tamirbssb@gmail.com>
2020-02-19 19:42:13 +0200

    khgv

--- js/main.js
+++ js/main.js
@@ -17,9 +17,9 @@ window.onload = () => {
         .catch(console.log('INIT MAP ERROR'));
 
     locService.getPosition()
-    .then(pos=>
-    console.log('User position is:', pos.coords))
-    .catch(err => {
+        .then(pos =>
+            console.log('User position is:', pos.coords))
+        .catch(err => {
             console.log('err!!!', err);
         })
 }
@@ -27,13 +27,20 @@ document.querySelector('.location-adress').addEventListener('click', () => {
     let addressFromUser = document.querySelector('.enter-location-input').value;
     mapService.codeAddress(addressFromUser)
         .then(res => {
-            // console.log(res.toJSON.lat, ' ', res.toJSON.lng)
-            // console.log(res)
-            // for (let key in res) {
-            //     console.log(key, ':', res[key]())
-            // }
             mapService.panTo(res.lat(), res.lng());
             mapService.addMarker({ lat: res.lat(), lng: res.lng() });
+            mapService.getWeather(res.lat(), res.lng())
+                .then(weather => {
+                    const temp = mapService.changeKelvinToCelsius(weather.main.temp);
+                    const tempMin = mapService.changeKelvinToCelsius(weather.main.temp_min);
+                    const tempMax = mapService.changeKelvinToCelsius(weather.main.temp_max);
+                    let strHTML = ``;
+                    strHTML += `
+                <p>weather(temp): ${temp}, min: ${tempMin} max: ${tempMax}</p>
+                <p>wind(speed):${weather.wind.speed} K/Hr</p>
+                `;
+                    document.querySelector('.weather').innerHTML = strHTML;
+                })
         });
 });
 
@@ -45,22 +52,35 @@ document.querySelector('.btn').addEventListener('click', (ev) => {
             console.log('coordsssss', coords);
             mapService.panTo(coords.latitude, coords.longitude);
             mapService.addMarker({ lat: coords.latitude, lng: coords.longitude });
+
+            mapService.getWeather(32.0149831, 34.4120554)
+                .then(weather => {
+                    const temp = mapService.changeKelvinToCelsius(weather.main.temp);
+                    const tempMin = mapService.changeKelvinToCelsius(weather.main.temp_min);
+                    const tempMax = mapService.changeKelvinToCelsius(weather.main.temp_max);
+                    let strHTML = ``;
+                    strHTML += `
+            <p>weather(temp): ${temp}, min: ${tempMin} max: ${tempMax}</p>
+            <p>wind(speed):${weather.wind.speed} K/Hr</p>
+            `;
+                    document.querySelector('.weather').innerHTML = strHTML;
+                })
         })
 })
 
 
-document.querySelector('.weather').addEventListener('click', (ev) => {
-    console.log('Aha!', ev.target);
-    mapService.getWeather(32.0149831, 34.4120554)
-        .then(weather => {
-            const temp = mapService.changeKelvinToCelsius(weather.main.temp);
-            const tempMin = mapService.changeKelvinToCelsius(weather.main.temp_min);
-            const tempMax = mapService.changeKelvinToCelsius(weather.main.temp_max);
-            let strHTML = ``;
-            strHTML += `
-            <p>weather(temp): ${temp}, min: ${tempMin} max: ${tempMax}</p>
-            <p>wind(speed):${weather.wind.speed} K/Hr</p>
-            `;
-            document.querySelector('.weather').innerHTML = strHTML;
-        })
-});
+// document.querySelector('.weather').addEventListener('click', (ev) => {
+//     console.log('Aha!', ev.target);
+//     mapService.getWeather(32.0149831, 34.4120554)
+//         .then(weather => {
+//             const temp = mapService.changeKelvinToCelsius(weather.main.temp);
+//             const tempMin = mapService.changeKelvinToCelsius(weather.main.temp_min);
+//             const tempMax = mapService.changeKelvinToCelsius(weather.main.temp_max);
+//             let strHTML = ``;
+//             strHTML += `
+//             <p>weather(temp): ${temp}, min: ${tempMin} max: ${tempMax}</p>
+//             <p>wind(speed):${weather.wind.speed} K/Hr</p>
+//             `;
+//             document.querySelector('.weather').innerHTML = strHTML;
+//         })
+// });
